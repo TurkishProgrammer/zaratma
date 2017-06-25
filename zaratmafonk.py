c67a879fcc97a74b1774366e2b2820f16f33e9ff
TurkishProgrammer <zekiberkeguncan@gmail.com>
2017-06-25 11:51:34 +0300

    Create zaratmafonk.py

---
+++ zaratmafonk.py
@@ -0,0 +1,4 @@
+import random
+def zar():
+    #random modülünün içindeki randrange komutuyla belirlediğim aralıktan rastgele bir sayı seçiyorum ve bunu return ile döndürüyorum.
+    return random.randrange(1,7)
